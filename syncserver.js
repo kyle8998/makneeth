9182eee4ce0a76ee9f74abe0b40336562b6cb0d2
Maxwell Newman <maxwellsawesome@gmail.com>
2019-12-04 20:37:48 -0500

    fixed kyles shit

--- syncserver.js
+++ syncserver.js
@@ -411,9 +411,9 @@ module.exports = {
 
 
 
-    server.listen(3000, function() {
-        console.log('Listening on port 3000!');
-    });
+    //server.listen(3000, function() {
+      //  console.log('Listening on port 3000!');
+    //});
 
 
   }
